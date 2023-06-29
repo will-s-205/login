0fbc5038d7a4f03ebfa091105c96d460a93e30ac
William S <will.step.205@gmail.com>
2023-06-29 16:50:07 -0700

    set goals

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +23,7 @@ function App() {
   );
 }
 
-export default App;
+// UI Sign up form
+// Back-End MongoDB Mongoose
+// Login
+  // Login as a guest feature
