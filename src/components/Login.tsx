140ace65d42dd5639a6a1785881c2f7137c87c55
William S <will.step.205@gmail.com>
2023-06-29 20:21:33 -0700

    initial form functions added

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import './Login.scss'
+import { useState, useEffect } from 'react'
+import '../assets/sass/Login.scss'
 
 export default function App() {
-  const [formData, setFormData] = React.useState({
-    email: "",
-    password: "",
-    passwordConfirm: "",
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    passwordConfirm: '',
     joinedNewsletter: false
   })
+
+    useEffect(() => {
+      console.log("useEffect")
+      localStorage.setItem("userCreds", JSON.stringify(formData))
+  }, [formData])
 
   function handleChange(event: { target: { name: any; value: any; type: any; checked: boolean } }) {
     const { name, value, type, checked } = event.target
@@ -17,29 +22,40 @@ export default function App() {
     }))
   }
 
+  function saveData() {
+    localStorage.setItem("userCreds", JSON.stringify({email: formData.email, password: formData.password}))
+    console.log(JSON.parse(localStorage.getItem("userCreds") || ""))
+  }
+
   function handleSubmit(event: { preventDefault: () => void }) {
     event.preventDefault()
-    if (formData.email === '') {
-      console.log("Enter your email")
-    } else if (formData.password === '') {
-      console.log("Enter new password")
-    } else if (formData.password !== formData.passwordConfirm) {
-      console.log("Passwords do not match")
-    } else if (formData.joinedNewsletter) {
-      console.log("Thanks for signing up for our newsletter!")
-    } else {
-      console.log("Successfully signed up")
+    if (formData.email === '' && formData.email === "") {
+      console.log("Please enter your email")
+      return
     }
+    else if (formData.password === '') {
+      console.log("Please enter new password")
+      return
+    }
+    else if (formData.password !== formData.passwordConfirm) {
+      console.log("Passwords do not match")
+      return
+    }
+    else
+      if (formData.joinedNewsletter)
+        console.log("Thanks for signing up for our newsletter!")
+    console.log("Successfully signed up")
+    saveData();
   }
 
   return (
     <div className="form-container">
-      <form className="form" onSubmit={handleSubmit}>
+      <form className="form" onSubmit={handleSubmit} action="api/creds" method="POST">
         <input
-          type="email"
+          type='email'
           placeholder="Email address"
           className="form--input"
-          name="email"
+          name='email'
           onChange={handleChange}
           value={formData.email}
         />
@@ -59,10 +75,9 @@ export default function App() {
           onChange={handleChange}
           value={formData.passwordConfirm}
         />
-
         <div className="form--marketing">
           <input
-            id="okayToEmail"
+            id='email-okay'
             type="checkbox"
             name="joinedNewsletter"
             onChange={handleChange}
