0dba67df614e6e984270c0ac048c2a123c92a19f
William S <will.step.205@gmail.com>
2023-06-29 17:09:01 -0700

    import sign up form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import './App.scss';
 import Login from './components/Login';
 
 export default function App() {
   return (
     <div className="App">
-<Login />
+      <Login />
     </div>
   );
 }
@@ -12,4 +13,4 @@ export default function App() {
 // UI Sign up form
 // Back-End MongoDB Mongoose
 // Login
-  // Login as a guest feature
+    // Login as a guest feature
