9f406fd4cc9c81d9146a6c1b78bc28d16e4b32ae
William S <will.step.205@gmail.com>
2023-07-02 08:45:34 -0700

    port removed

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -46,9 +46,8 @@ export default function App() {
         console.log("Thanks for signing up for our newsletter!")
     console.log("Wait... signing up")
 
-    const url2: string = 'http://localhost:4000/api/signup'
-    const url: string = 'https://login.rigo205.repl.co/api/signup'
-    const response = await fetch(url2, {
+    const url: string = 'https://server.rigo205.repl.co/api/signup'
+    const response = await fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
